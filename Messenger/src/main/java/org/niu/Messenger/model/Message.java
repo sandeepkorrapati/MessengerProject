615b73be939b63821cdccffd08c9466f64e9cf51
sandeepkorrapati <sandeep.korrapati111@gmail.com>
2018-08-13 10:12:11 -0500

    Added Message POJO

---
+++ Messenger/src/main/java/org/niu/Messenger/model/Message.java
@@ -0,0 +1,67 @@
+package org.niu.Messenger.model;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
+@XmlRootElement
+public class Message {
+	
+	private long id;
+	private String message;
+	private String author;
+	private Date created;
+	private Map<Long, Comment> comments = new HashMap<>();
+	
+	
+	public Message() {
+		super();
+	}
+	
+	public Message(long id, String message, String author) {
+		super();
+		this.id = id;
+		this.message = message;
+		this.author = author;
+		this.created = new Date();
+	}
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public Date getCreated() {
+		return created;
+	}
+	public void setCreated(Date created) {
+		this.created = created;
+	}
+	
+	@XmlTransient
+	public Map<Long, Comment> getComments() {
+		return comments;
+	}
+
+	public void setComments(Map<Long, Comment> comments) {
+		this.comments = comments;
+	}
+	
+	
+}
